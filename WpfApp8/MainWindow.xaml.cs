b8d48e59cd95f2c97acd8aff683229f4c078eda9
Baiyun-u-smartAI <45277409+Baiyun-u-smartAI@users.noreply.github.com>
2022-12-04 21:30:39 +0800

    闪图 多线程延时

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Timer timer;
+        private bool changed = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new Timer(1000);
+            timer.AutoReset = true;
+            timer.Start();
+            timer.Elapsed += OnTimedEvent;
+
+
+        }
+
+        private void thisImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            timer.Stop();
+            timer.AutoReset = false;
+
+        }
+
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            // Updating the Label which displays the current second
+
+
+            this.Dispatcher.Invoke(
+                    new Action(() =>
+                    {
+                        if (changed) thisImage.Source = new BitmapImage(new Uri(@"/WpfApp8;component/Images/darknet.png", UriKind.Relative));
+                        else thisImage.Source = new BitmapImage(new Uri("./Images/logo.jpg", UriKind.Relative));
+                        changed = !changed;
+                    }
+                    )
+                );
+
+
+        }
+
+
+    }
+}
