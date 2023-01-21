24eeb056ea1121d544398e1576bda0c608820bcc
Baiyun-u-smartAI <45277409+Baiyun-u-smartAI@users.noreply.github.com>
2023-01-21 11:32:41 +0800

    opencvsharp 和 wpf 结合，使用Mahapps和子线程异步更新UI

---
+++ opencv_wpf2/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using MahApps.Metro.Controls;
+
+namespace opencv_wpf2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private Mat src;
+        private Thread action;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.src = Cv2.ImRead("aec655d5c73d4167942ad8bc409733d0.png");
+            ImageCV.Source = this.src.ToWriteableBitmap();
+            
+
+        }
+
+        private void BrightnessBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            status.Content = new ProgressRing
+            {
+                Width = 25,
+                Height = 25
+            };
+            //Console.WriteLine(e.NewValue);
+            //int val = (int)e.NewValue;
+            action = new Thread(ChangeBrightness); // 在子线程运行更新UI（异步）
+            action.Start();
+            
+        }
+
+        private void ChangeBrightness()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                int val = (int)BrightnessBar.Value;
+                Mat dst = this.src + new Scalar(val, val, val);
+                ImageCV.Source = dst.ToWriteableBitmap();
+                if (val > 0)
+                {
+                    status.Content = "亮度增强 +" + val.ToString();
+                }
+                else if (val < 0)
+                {
+                    status.Content = "亮度减弱 " + val.ToString();
+                }
+                else
+                {
+                    status.Content = "原图";
+                }
+                
+            }
+            )
+                );
+        }
+    }
+}
